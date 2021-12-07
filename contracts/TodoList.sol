d5caa5d2d7b2130814987fd8100ec7cafe4a5ecc
Jimmy <jgarcia3000@gmail.com>
2021-12-07 15:34:36 -0800

    created function to create a task, added a constructor with a default task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,20 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
   uint public taskCount;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    createTask("Eat lemons.");
+  }
+  function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
